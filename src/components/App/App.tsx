d948e1b8e20dde40985f478bca06e1ec949165f5
William Owen <git@wo.dev>
2022-04-14 14:22:03 +0100

    Basic pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,22 @@
 
-import "./App.css"
+import style from "./App.module.sass"
 import { Routes, Route, Link } from "react-router-dom"
+import PageDiscordTime from "../../pages/PageDiscordTime"
 
 function App() {
 
 	return (
-		<div className="App">
+		<div className={style.App}>
+
 			<nav>
-				<Link to="/about">About</Link>
+				<Link to="/discord-time">Discord Time</Link>
 				<Link to="/">Home</Link>
 			</nav>
+
 			<main>
 				<Routes>
 					<Route path="/" element={<h1>Home</h1>} />
-					<Route path="about" element={<h1>About</h1>} />
+					<Route path="/discord-time" element={<PageDiscordTime/>} />
 				</Routes>
 			</main>
 
---
+++ src/pages/PageDiscordTime/PageDiscordTime.tsx
@@ -0,0 +1,17 @@
+import style from "./PageDiscordTime.module.sass"
+
+const PageDiscordTime: React.FC = () => {
+
+	return (
+
+		<div data-testid="Page-PageDiscordTime" className={style.PageDiscordTime}>
+
+			<h1>Discord Time</h1>
+
+		</div>
+
+	)
+
+}
+
+export default PageDiscordTime
