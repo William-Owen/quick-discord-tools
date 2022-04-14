f673bf398b4a6db8813bbb2a6f8250ae3b09fe8e
William Owen <git@wo.dev>
2022-04-14 12:47:11 +0100

    Implemented react router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,22 @@
-import React from "react"
-import logo from "./logo.svg"
+
 import "./App.css"
+import { Routes, Route, Link } from "react-router-dom"
 
 function App() {
 
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+			<nav>
+				<Link to="/about">About</Link>
+				<Link to="/">Home</Link>
+			</nav>
+			<main>
+				<Routes>
+					<Route path="/" element={<h1>Home</h1>} />
+					<Route path="about" element={<h1>About</h1>} />
+				</Routes>
+			</main>
+
 		</div>
 	)
 
